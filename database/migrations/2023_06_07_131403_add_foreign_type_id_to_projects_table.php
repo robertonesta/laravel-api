0aceeaf26b840796bb20f8a1fc58dfc58abad774
RobertoNesta <robertonesta1993@gmail.com>
2023-06-07 15:24:40 +0200

    fill up and down methods in the foreign key table

--- database/migrations/2023_06_07_131403_add_foreign_type_id_to_projects_table.php
+++ database/migrations/2023_06_07_131403_add_foreign_type_id_to_projects_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+
+            $table -> unsignedBigInteger('type_id')->nullable()->after('id');
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
+
         });
     }
 
@@ -26,7 +29,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropForeign('projects_type_id_foreign');
+            $table->dropColumn('type_id');
         });
     }
 };
