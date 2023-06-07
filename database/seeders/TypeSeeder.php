431a3fd97acae7bdb954d53fd2fb29fdddd7ee44
RobertoNesta <robertonesta1993@gmail.com>
2023-06-07 15:14:36 +0200

    create a new type migration

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Type;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = [
+            "Front End",
+            "Back End",
+            "Full Stack"
+        ];
+        foreach ($types as $type) {
+            $newType = New Type();
+            $newType -> name = $type;
+            $newType -> save();
+        }
+    }
+}
